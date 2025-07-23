9211ade48dab263212a6ac385e06975d419dad6c
andGarciaQ <andres.garcia@quind.io>
2025-07-22 21:51:09 -0500

    feat | refactor: Se implementa correctamente el ednpoint personalizado de actuator

--- src/main/java/com/demo/advanced/controller/RateLimitCapacityResource.java
+++ src/main/java/com/demo/advanced/controller/RateLimitCapacityResource.java
@@ -1,14 +1,17 @@
 package com.demo.advanced.controller;
 
 import com.demo.advanced.controller.ratelimit.SearchBuckets;
+import com.demo.advanced.dto.response.BucketCapacityResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Profile("dev")
 @Endpoint(id = "ratelimit")
 @RequiredArgsConstructor
 public class RateLimitCapacityResource {
@@ -16,8 +19,8 @@ public class RateLimitCapacityResource {
     private final SearchBuckets searchBuckets;
 
     @ReadOperation
-    public List<String> customEndpoint() {
-        return searchBuckets.getAllBucketKeys();
+    public List<BucketCapacityResponse> customEndpoint() {
+        return searchBuckets.getAllBucketsAvailableTokens();
     }
 
 }
--- src/main/java/com/demo/advanced/controller/ratelimit/SearchBuckets.java
+++ src/main/java/com/demo/advanced/controller/ratelimit/SearchBuckets.java
@@ -1,5 +1,7 @@
 package com.demo.advanced.controller.ratelimit;
 
+import com.demo.advanced.dto.response.BucketCapacityResponse;
+import io.github.bucket4j.Bucket;
 import io.lettuce.core.KeyScanCursor;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
@@ -7,28 +9,37 @@ import io.lettuce.core.api.sync.RedisCommands;
 import io.lettuce.core.codec.ByteArrayCodec;
 import io.lettuce.core.codec.RedisCodec;
 import io.lettuce.core.codec.StringCodec;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 public class SearchBuckets {
 
     private final RedisClient redisClient;
+    private final Map<RateLimitType, RateLimitStrategy> strategies;
 
-    public List<String> getAllBucketKeys() {
+    public SearchBuckets(RedisClient redisClient, final List<RateLimitStrategy> strategiesBeans) {
+        this.strategies = strategiesBeans.stream().collect(Collectors.toMap(RateLimitStrategy::getType, Function.identity()));
+        this.redisClient = redisClient;
+    }
 
-        final List<String> keys = new ArrayList<>();
+    public List<BucketCapacityResponse> getAllBucketsAvailableTokens() {
 
-        try (StatefulRedisConnection<String, byte[]> connection = redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE))) {
+        final Set<String> keys;
 
-            final RedisCommands<String, byte[]> syncCommands = connection.sync();
+        try (StatefulRedisConnection<String, byte[]> redisConnection = redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE))) {
+
+            final RedisCommands<String, byte[]> syncCommands = redisConnection.sync();
 
             KeyScanCursor<String> cursor = syncCommands.scan();
-            keys.addAll(cursor.getKeys());
+            keys = new HashSet<>(cursor.getKeys());
 
             while (!cursor.isFinished()) {
                 cursor = syncCommands.scan(cursor);
@@ -37,7 +48,20 @@ public class SearchBuckets {
 
         }
 
-        return keys;
+        final List<BucketCapacityResponse> bucketsWithCapacity = new ArrayList<>(keys.size());
+
+        keys.forEach(key -> {
+
+            final RateLimitType type = RateLimitType.valueOf(key.split("-")[0]);
+
+            final Bucket bucket = strategies.get(type).getOrCreateBucket(key);
+
+            bucketsWithCapacity.add(new BucketCapacityResponse(key, bucket.getAvailableTokens()));
+
+        });
+
+
+        return bucketsWithCapacity;
     }
 
 }
