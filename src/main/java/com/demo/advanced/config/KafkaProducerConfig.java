182da59a63dffdcbc7c79f154ef7fac6f2485a8a
andGarciaQ <andres.garcia@quind.io>
2026-01-03 22:15:13 -0500

    chore: Configuracion para la conexion con kafka y la generacion de clases automaticamente segun el schema avro

---
+++ src/main/java/com/demo/advanced/config/KafkaProducerConfig.java
@@ -0,0 +1,59 @@
+package com.demo.advanced.config;
+
+import com.demo.advanced.dto.event.TransactionExternalEvent;
+import io.apicurio.registry.resolver.config.SchemaResolverConfig;
+import io.apicurio.registry.serde.avro.AvroKafkaSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${apicurio.registry.url}")
+    private String registryUrl;
+
+    @Bean
+    public ProducerFactory<String, TransactionExternalEvent> producerFactory() {
+
+        final Map<String, Object> props = new HashMap<>();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroKafkaSerializer.class);
+
+        // Configuración Apicurio
+        props.put(SchemaResolverConfig.REGISTRY_URL, registryUrl);
+        props.put(SchemaResolverConfig.AUTO_REGISTER_ARTIFACT, Boolean.TRUE);
+        props.put(SchemaResolverConfig.AUTO_REGISTER_ARTIFACT_IF_EXISTS, "CREATE_VERSION");
+        props.put(SchemaResolverConfig.ARTIFACT_RESOLVER_STRATEGY, "io.apicurio.registry.serde.strategy.TopicIdStrategy");
+
+        // Evitar duplicados
+        props.put("apicurio.registry.serde.check-registry-state", "true");
+        props.put("apicurio.registry.serde.avro-encoding", "BINARY");
+
+        // Asignacion de esquema especifico
+        props.put(SchemaResolverConfig.EXPLICIT_ARTIFACT_GROUP_ID, "transactions");
+        props.put(SchemaResolverConfig.EXPLICIT_ARTIFACT_ID, "transactions-events");
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, TransactionExternalEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
